643ecae202537b07ce812ccff26c76bd3d227ec3
codeworm96 <codeworm96@outlook.com>
2017-06-23 16:45:16 +0800

    Replace dummy model with real model

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from flask import Flask, jsonify
+from analyze import analyze
 import requests
 import sys
 app = Flask(__name__)
 
 DB_URL = sys.argv[1]
 
-def sentiment_analyze(batch, mode):
-    return [1] * len(batch)
-
 @app.route('/analyze/<int:uid>')
-def analyze(uid):
+def analyze_endpoint(uid):
     r = requests.get(DB_URL + str(uid))
     weibo = r.json()
     batch = []
     for w in weibo:
         batch.append(w['content'])
         batch.extend([[c] for c in w['comments']])
-    res = sentiment_analyze(batch, 1)
+    res = [float(r) for r in analyze(batch, 0)]
     for w in weibo:
         w['content_sentiment'] = res[0]
         res = res[1:]
