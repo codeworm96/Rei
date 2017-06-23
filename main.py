f6e0b89340f6f16a57bc613d906ed6caa76344ce
codeworm96 <codeworm96@outlook.com>
2017-06-23 13:16:29 +0800

    Add analyze API

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+import requests
+import sys
+app = Flask(__name__)
+
+DB_URL = sys.argv[1]
+
+def sentiment_analyze(batch, mode):
+    return [1] * len(batch)
+
+@app.route('/analyze/<int:uid>')
+def analyze(uid):
+    r = requests.get(DB_URL + str(uid))
+    weibo = r.json()
+    batch = []
+    for w in weibo:
+        batch.append(w['content'])
+        batch.extend([[c] for c in w['comments']])
+    res = sentiment_analyze(batch, 1)
+    for w in weibo:
+        w['content_sentiment'] = res[0]
+        res = res[1:]
+        l = len(w['comments'])
+        w['comments_sentiment'] = res[0:l]
+        res = res[l:]
+    return jsonify(weibo)
+
+if __name__ == '__main__':
+    app.run()
